a540cca564d7d3a24fa528c85e5f04f83e48fa0e
Megan Yu <myu55@umd.edu>
2025-03-29 17:23:10 -0400

    New map so we can use Markers

--- hack4thon-proj/src/App.js
+++ hack4thon-proj/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+import {AdvancedMarker, APIProvider, Map} from '@vis.gl/react-google-maps';
+
 
 const containerStyle = {
   width: '100%',
@@ -13,39 +14,17 @@ const center = {
 }
 
 function App() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: 'AIzaSyCOow_U3fskeHmVl-nxfHl8lniBjOlOIXM',
-  })
+  const position = {lat: 38.91115, lng: -77.02963};
 
-  const [map, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center)
-    map.fitBounds(bounds)
-
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={11}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      {/* Child components, such as markers, info windows, etc. */}
-      <></>
-    </GoogleMap>
-  ) : (
-    <></>
+  return (
+    <APIProvider apiKey=''>
+      <Map defaultCenter={position} defaultZoom={13} mapId="dmv_map" style={{ width: '100%', height: '100vh' }}>
+        <AdvancedMarker position={position} />
+      </Map>
+    </APIProvider>
   );
 }
 
-export default React.memo(App)
+// export default React.memo(App)
+export default App;
+
