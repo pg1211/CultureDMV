8a418e930e37b99fef0dfd46309b19f8a6be7bdb
heavens-potato <sswu2005@gmail.com>
2025-03-30 12:18:13 -0400

    Added profile icon next to search

--- hack4thon-proj/src/App.js
+++ hack4thon-proj/src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps';
 import { TextField, Box, InputAdornment, styled } from '@mui/material';
 import { Search } from '@mui/icons-material';
+import FaceIcon from '@mui/icons-material/Face';
 
 const containerStyle = {
   width: '100%',
@@ -19,43 +20,56 @@ function App() {
 
   const StyledTextField = styled(TextField)({
     '& .MuiFilledInput-root': {
-      backgroundColor: 'white', 
+      backgroundColor: 'white',
       borderRadius: '50px',
       border: 'none',
-      '&::before': { border: 'none' }, 
-      '&::after': { border: 'none' }, 
-      '&:before': { border: 'none' }, 
+      '&::before': { border: 'none' },
+      '&::after': { border: 'none' },
+      '&:before': { border: 'none' },
       boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
       outline: 'none',
+      '&:hover': { 
+      backgroundColor: 'white',
+    },
     },
   });
 
   return (
-    <APIProvider apiKey={process.env.APIKey}>
-      <Box sx={{ position: 'relative', height: '100vh', width: '100%', display: 'flex', justifyContent: 'center' }}>
-        <StyledTextField
-          id="filled-basic"
-          label="Search for events, classes, cuisine..."
-          variant="filled"
-          sx={{
-            width: '80%',
-            position: 'absolute',
-            top: '20px',
-            left: '20px',
-            zIndex: 10,
-            display: 'flex',
-            left: '50%', 
-            transform: 'translateX(-50%)', 
-          }}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <Search />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <Map defaultCenter={position} defaultZoom={13} mapId="dmv_map" style={{ width: '100%', height: '100%' }} mapTypeControl={false}>
+    <APIProvider apiKey={process.env.REACT_APP_API_KEY}>
+      <Box sx={{ position: 'relative', height: '100vh', width: '100%' }}>
+        <Box sx={{
+          position: 'absolute',
+          top: '20px',
+          left: '50%',
+          transform: 'translateX(-50%)',
+          zIndex: 10,
+          display: 'flex',
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%'
+        }}>
+          <FaceIcon sx={{ marginRight: '20px', fontSize: '3.5rem', backgroundColor: 'white', borderRadius: '50px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', }} />
+          <StyledTextField
+            id="filled-basic"
+            label="Search for events, classes, cuisine..."
+            variant="filled"
+            sx={{
+              width: '80%',
+            }}
+            InputProps={{
+              inputProps: {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Search />
+                  </InputAdornment>
+                ),
+              },
+            }}
+          />
+        </Box>
+        <Map defaultCenter={position} defaultZoom={13} mapId="dmv_map" style={{ width: '100%', height: '100%' }} mapTypeControl={false} streetViewControl={false}>
           <AdvancedMarker position={position} />
         </Map>
       </Box>
